cea9c81c6ca8e9642243fd8f88fa48f9f4e6f69a
hemanginigohel <hemangini.gohel@bacancy.com>
2020-12-28 10:03:23 +0530

    added developer name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   activeYear: any = "";
   activeLaunch: any = "";
   activeLand: any = "";
+  developdBy = "Hemangini Gohel";
 
   ngOnInit(): void {
     this.getSpecxData();
